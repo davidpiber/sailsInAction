23be9c1e7893d4763a57e7ce05d3c9b8a0ac34ec
David Bermudez <davidpiber@hotmail.com>
2017-09-02 15:49:13 -0600

    changes for brushfire

---
+++ brushfire/assets/js/videos-page.js
@@ -0,0 +1,22 @@
+$(function whenDomIsReady(){
+    setTimeout(function afterRetrievingVieos() {
+        var videos = [
+            {
+                title: 'JOURNEY TO JAH - Gentleman Ft. Alborosie',
+                src: 'https://www.youtube.com/watch?v=dN8FTAx06rE&list=RDdN8FTAx06rE'
+            },
+            {
+                title: 'Suga Roy & Conrad Crystal feat. Alborosie - Run Come',
+                src: 'https://www.youtube.com/watch?v=JEXDg8fz8uI&index=2&list=RDdN8FTAx06rE'
+            }
+        ];
+    });
+});
+
+$('he-list-of-videos .loading').hide();
+
+var videoHtml = _.reduce(videos, function(html, video){
+    html = '<li class="video">' +
+         ' <h2>' + video.title + '</h2>' +
+         ' <iframe width="640" height="390" src="'+ video.src +'"frameborder="0">'
+});
