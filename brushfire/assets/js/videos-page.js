f973bcb17e8752936f647296c20e70a5a1a4c22c
David Bermudez <davidpiber@hotmail.com>
2017-09-02 16:14:35 -0600

    changes in videos page

--- brushfire/assets/js/videos-page.js
+++ brushfire/assets/js/videos-page.js
@@ -10,13 +10,22 @@ $(function whenDomIsReady(){
                 src: 'https://www.youtube.com/watch?v=JEXDg8fz8uI&index=2&list=RDdN8FTAx06rE'
             }
         ];
-    });
+
+    $('the-list-of-videos .loading').hide();
+
+    var videosHtml = _.reduce(videos, function(html, video) {
+        html = '<li class="video">' +
+            ' <h2>' + video.title + '</h2>' +
+            ' <iframe width="640" height="390" src="'+ video.src +'"frameborder="0" allowfullscreen></iframe>' +
+            '</li>';
+        return html;
+    },'');
+
+    $('the-list-of-videos ul').replaceWith(videosHtml);
+
+    }, 750);
+
 });
 
-$('he-list-of-videos .loading').hide();
 
-var videoHtml = _.reduce(videos, function(html, video){
-    html = '<li class="video">' +
-         ' <h2>' + video.title + '</h2>' +
-         ' <iframe width="640" height="390" src="'+ video.src +'"frameborder="0">'
-});
+
