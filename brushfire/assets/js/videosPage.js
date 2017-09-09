70ffa5bd5de6d40fc45baf9548eaa085e1a2745e
David Bermudez <davidpiber@hotmail.com>
2017-09-09 09:20:14 -0600

    angular api

--- brushfire/assets/js/videosPage.js
+++ brushfire/assets/js/videosPage.js
@@ -1,27 +1,86 @@
-$(document).ready(function() {
-    setTimeout(function afterRetrievingVieos() {
-        var videos = [
-            {
-                title: 'JOURNEY TO JAH - Gentleman Ft. Alborosie',
-                src: 'https://www.youtube.com/embed/4uKX5CS75fA'
-            }
-        ];
+angular.module('brushfire_videosPage', [])
+.config(['$sceDelegateProvider', function($sceDelegateProvider) {
+$sceDelegateProvider.resourceUrlWhitelist([
+  'self',
+  '*://www.youtube.com/**'
+]);
+}]);
 
-    $('the-list-of-videos .loading').hide();
+angular.module('brushfire_videosPage').controller('PageCtrl', [
+'$scope', '$http',
+function($scope, $http) {
+  $scope.videosLoading = true;
 
-    var videosHtml = _.reduce(videos, function(html, video) {
-        html = '<li class="video">' +
-            ' <h2>' + video.title + '</h2>' +
-            ' <iframe width="640" height="390" src="'+ video.src +'"frameborder="0" allowfullscreen></iframe>' +
-            '</li>';
-        return html;
-    },'');
+  $scope.submitVideosError = false;
 
-    $('.the-list-of-videos ul').replaceWith(videosHtml);
+  // Get the existing videos.
+  $http.get('/video')
+    .then(function onSuccess(sailsResponse) {
+      $scope.videos = sailsResponse.data;
+    })
+    .catch(function onError(sailsResponse) {
 
-    }, 750);
+      if (sailsResponse.data.status === '404') {
+        return;
+      }
 
-});
+      console.log("An unexpected error occurred: " + sailsResponse.data.statusText);
+
+    })
+    .finally(function eitherWay() {
+      $scope.videosLoading = false;
+    });
+
+  $scope.submitNewVideo = function() {
+
+    // A little "spin-lock" to prevent double-submission
+    // (because disabling the submit button still allows double-posts
+    //  if a user hits the ENTER key to submit the form multiple times.)
+    if ($scope.busySubmittingVideo) {
+      return;
+    }
+
+    // Harvest the data out of the form
+    // (thanks to ng-model, it's already in the $scope object)
+    var _newVideo = {
+      title: $scope.newVideoTitle,
+      src: $scope.newVideoSrc,
+    };
+
+    // create placeholder anchor element
+    var parser = document.createElement('a');
+
+    // assign url to parser.href
+    parser.href = _newVideo.src
+
+    // Use the indexOf parser.search as the first argument and length of
+    // parser.search as the second argument of parser.search.substring
+    // The result is the YouTube ID --> LfOWehvvuo0
+    var youtubeID = parser.search.substring(parser.search.indexOf("=") + 1, parser.search.length);
+
+    _newVideo.src = 'https://www.youtube.com/embed/' + youtubeID;
 
 
 
+    // First, show a loading state
+    // (also disables form submission)
+    $scope.busySubmittingVideo = true;
+
+    $http.post('/video', {
+      title: _newVideo.title,
+      src: _newVideo.src
+    })
+    .then(function onSuccess(sailsResponse) {
+      $scope.videos.unshift(_newVideo);
+    })
+    .catch(function onError(sailsResponse) {
+      console.log("An unexpected error occurred: " + sailsResponse.data.statusText);
+    })
+    .finally(function eitherWay() {
+      $scope.busySubmittingVideo = false;
+      $scope.newVideoTitle = '';
+      $scope.newVideoSrc = '';
+    });
+  };
+}
+]);
